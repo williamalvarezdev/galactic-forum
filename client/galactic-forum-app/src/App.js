f81810c76f31b2832e940aef7ad3df8fdb509733
Luis Munoz <lmunozi181i@gmail.com>
2024-03-30 06:44:51 -0700

    I setup the react project for client and server side using express. Also client side is running on port300 by defaultand server side 5000. Additionally, to start client cd into client => galactic-forum-app. for server => server. npm start

---
+++ client/galactic-forum-app/src/App.js
@@ -0,0 +1,58 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() =>{
+    fetch("/api").then((response) =>response.json()).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+    //Passing empty array so that it only renders once per request
+  }, [])
+
+  return (
+    <div>
+
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ): (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+      
+    </div>
+  )
+}
+
+export default App
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;

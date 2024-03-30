d40269bcc154709ab3d8194b209772fa4d53da4d
Yaksh Soni <66526673+Ysoni18@users.noreply.github.com>
2024-03-30 19:06:05 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,22 +9,3 @@ app.get("/api", (req, res) => {
 app.listen(5000, () => {
     console.log("Server listening on port 5000");
 })
-
-const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
-const uri = "mongodb+srv://williamalvarezdev:Qrfv8QSmmfQrreHx@cluster0.iufiwin.mongodb.net/";
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    const database = client.db('forumchain');
-    const movies = database.collection('username');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { user: 'test0' };
-    const movie = await username.findOne(query);
-    console.log(username);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
